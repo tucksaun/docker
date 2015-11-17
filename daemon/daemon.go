574bc5caef90268db293c2a4ecb2065b87999c61
Tugdual Saunier <tugdual.saunier@gmail.com>
2015-11-17 13:33:27 +0000

    -

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -707,8 +707,8 @@ func NewDaemon(config *Config, registryService *registry.Service) (daemon *Daemo
 
 	sysInfo := sysinfo.New(false)
 	// Check if Devices cgroup is mounted, it is hard requirement for container security,
-	// on Linux/FreeBSD.
-	if runtime.GOOS != "windows" && !sysInfo.CgroupDevicesEnabled {
+	// on Linux.
+	if runtime.GOOS == "linux" && !sysInfo.CgroupDevicesEnabled {
 		return nil, fmt.Errorf("Devices cgroup isn't mounted")
 	}
 
