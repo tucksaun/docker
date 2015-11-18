5d252acb346ed5c7d51c873516bcab8c89f53d48
Tugdual Saunier <tugdual.saunier@gmail.com>
2015-11-18 19:07:11 +0000

    Does it compile?

--- vendor/src/github.com/docker/libnetwork/drivers_freebsd.go
+++ vendor/src/github.com/docker/libnetwork/drivers_freebsd.go
@@ -3,9 +3,6 @@ package libnetwork
 import (
 	"github.com/docker/libnetwork/driverapi"
 	"github.com/docker/libnetwork/drivers/bridge"
-	"github.com/docker/libnetwork/drivers/host"
-	"github.com/docker/libnetwork/drivers/null"
-	"github.com/docker/libnetwork/drivers/remote"
 )
 
 func initDrivers(dc driverapi.DriverCallback) error {
