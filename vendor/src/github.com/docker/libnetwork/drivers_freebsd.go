86ab654bf78ec51281c635e63ef99946747990c1
Tugdual Saunier <tugdual.saunier@gmail.com>
2015-11-18 19:05:48 +0000

    Does it compile?

--- vendor/src/github.com/docker/libnetwork/drivers_freebsd.go
+++ vendor/src/github.com/docker/libnetwork/drivers_freebsd.go
@@ -11,9 +11,6 @@ import (
 func initDrivers(dc driverapi.DriverCallback) error {
 	for _, fn := range [](func(driverapi.DriverCallback) error){
 		bridge.Init,
-		host.Init,
-		null.Init,
-		remote.Init,
 	} {
 		if err := fn(dc); err != nil {
 			return err
